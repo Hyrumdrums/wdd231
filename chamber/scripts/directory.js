a89f206c1735802bab8523de6727be30227c11d0
Dallin Layton <hyrumdrums@gmail.com>
2026-03-18 11:45:57 -0600

    add members.json and images

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,43 @@
+const url = './data/members.json';
+
+const cards = document.querySelector('#cards');
+
+function displayMembers(members) {
+    members.forEach((member) => {
+        const card = document.createElement('section');
+        
+        const name = document.createElement('h2');
+        name.textContent = member.name;
+
+        const detailsElement = document.createElement('p');
+        detailsElement.innerHTML = `Address: ${member.address}`;
+        detailsElement.innerHTML += `<br>Phone: ${member.phone}`;
+        detailsElement.innerHTML += `<br>Website: <a href="${member.website}" target="_blank">${member.website}</a>`;
+        
+        const image = document.createElement('img');
+        image.setAttribute('src', `./images/members/${member.image}`);
+        image.setAttribute('alt', `${member.name} logo`);
+        image.setAttribute('loading', 'lazy');
+        image.setAttribute('width', '200');
+        image.setAttribute('height', '200');
+        
+        card.appendChild(name);
+        card.appendChild(detailsElement);
+        card.appendChild(image);
+
+        cards.appendChild(card);
+
+    });
+}
+
+async function getMembers() {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        displayMembers(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+getMembers();
